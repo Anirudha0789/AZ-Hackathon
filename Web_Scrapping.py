0e0164aaaa62e4087bef857fbff9f46fcbaeca50
Anirudha0789 <anirudha0789@gmail.com>
2022-05-13 09:09:57 +0530

    13-May-2022

--- Web_Scrapping.py
+++ Web_Scrapping.py
@@ -82,13 +82,10 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
-from random import randint
-from time import sleep
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# driver.get("https://www.codechef.com/tags/problems/dynamic-programming")
-driver.get("https://www.codechef.com/tags/problems/graphs")
+driver.get("https://www.codechef.com/tags/problems/array")
 
 time.sleep(5)
 
@@ -112,19 +109,16 @@ for ques in all_ques:
     urls.append("https://www.codechef.com"+ques['href'])
     title.append(ques.text)
 
-with open("problem_urls_graphs.txt", "w+") as f:
+with open("problem_urls_array.txt", "w+") as f:
     f.write('\n'.join(urls))
 
-with open("problem_titles_graphs.txt", "w+") as f:
+with open("problem_titles_array.txt", "w+") as f:
     f.write('\n'.join(title))
 
 
 # Creating New Folder
-# new_folder = r'DP_problems'
-# if not os.path.exists(new_folder):
-#     os.makedirs(new_folder)
 
-new_folder = r'Graphs_problems'
+new_folder = r'Array_problems'
 if not os.path.exists(new_folder):
     os.makedirs(new_folder)
 
@@ -138,8 +132,7 @@ for url in urls:
     problem_text = soup.find("div", {"class" : "problem-statement"}).get_text()
 
     # File name with the title.
-
-    with open("Graphs_problems/"+title[count]+".txt", "w+", encoding="utf-8") as f:
+    with open("Array_problems/"+title[count]+".txt", "w+", encoding="utf-8") as f:
         txt = problem_text
         # txt = re.sub(r'\n\s*\n', '\n', txt)
         # Adds two blanks between all paragraphs
@@ -148,17 +141,3 @@ for url in urls:
         txt = re.sub(r'\n*\Z', '', txt)
         f.write(txt)
     count+=1
-
-# for cnt in range(1,2):
-# with open("DP_problems/Chef and String - XYSTR.txt", "r+", encoding="utf-8") as f:
-#     txt = f.read()
-#     txt = re.sub(r'\n\s*\n', '\n', txt)
-#     # Adds two blanks between all paragraphs
-#     txt = re.sub(r'\n', '\n\n', txt)
-#     # Removes the blank lines from the EOF
-#     txt = re.sub(r'\n*\Z', '', txt)
-#     # Writes to file and closes
-#     f.truncate()
-#     f.write("modified")
-#     f.write(txt)
-#     f.close()
